b3e61ba47fe30cb1c44340737f2e2d38099c25a1
SpyrKarv <sp.karvouniaris@yahoo.gr>
2021-12-31 12:00:34 +0200

    Done (no comments, I'll do them someday)

--- RoundRobin.java
+++ RoundRobin.java
@@ -1,7 +1,8 @@
-
 public class RoundRobin extends Scheduler {
 
     private int quantum;
+    private Process currentProcess;
+    private int process_timer;
     
     public RoundRobin() {
         this.quantum = 1; // default quantum
@@ -15,11 +16,31 @@ public class RoundRobin extends Scheduler {
 
     public void addProcess(Process p) {
         /* TODO: you need to add some code here */
+        processes.add(p);
+        for (int i = processes.size() - 2; i >= 0; i--)
+            processes.set(i + 1, processes.get(i));
+        processes.set(0, p);
     }
     
     public Process getNextProcess() {
         /* TODO: you need to add some code here
          * and change the return value */
-        return null;
+        if (processes.isEmpty())
+            return null;
+        else if (currentProcess == null) {
+            currentProcess = processes.get(processes.size() - 1);
+            process_timer = 1;
+        }
+        else if (process_timer == quantum || currentProcess.getPCB().getState() == ProcessState.TERMINATED) {
+            int pos = processes.indexOf(currentProcess);
+            if (pos - 1 >= 0)
+                currentProcess = processes.get(pos - 1);
+            else
+                currentProcess = processes.get(processes.size() - 1);
+            process_timer = 1;
+        }
+        else if (currentProcess.getPCB().getState() == ProcessState.RUNNING)
+            process_timer++;
+        return currentProcess;
     }
 }
