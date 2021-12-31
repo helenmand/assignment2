6694576fe0b76e0080441ee13eed70cff34e984c
Eleni <elemandana@gmail.com>
2021-12-31 12:32:52 +0200

    FCFS added

--- FCFS.java
+++ FCFS.java
@@ -7,11 +7,16 @@ public class FCFS extends Scheduler {
 
     public void addProcess(Process p) {
         /* TODO: you need to add some code here */
+        this.processes.add(p);
     }
     
     public Process getNextProcess() {
         /* TODO: you need to add some code here
          * and change the return value */
+        if (processes.size() > 0){
+            Process head = processes.get(0);
+            return head;
+        }
         return null;
     }
 }
