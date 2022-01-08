9de775f320bf1cc7907583b300fe261c3a98a5d5
Christos Apostolidis <chriapos@csd.auth.gr>
2022-01-08 23:55:12 +0200

    fixed issues with firstfit and added testing code

--- FirstFit.java
+++ FirstFit.java
@@ -16,8 +16,8 @@ public class FirstFit extends MemoryAllocationAlgorithm {
 
         ArrayList<Integer> startFlag = new ArrayList<Integer>();
         ArrayList<Integer> endFlag = new ArrayList<Integer>();
-        int currentBlockStart;
-        int currentBlockEnd;
+        int currentBlockStart=0;
+        int currentBlockEnd=0;
 
         //Check every block, quit if process fits
         for(int i=0;i<availableBlockSizes.length && !fit;i++) {
@@ -29,49 +29,78 @@ public class FirstFit extends MemoryAllocationAlgorithm {
             }
             currentBlockEnd = currentBlockStart+availableBlockSizes[i]-1;
 
-            //Checks if there are any slots
-            if(currentlyUsedMemorySlots.isEmpty()) {
+         
+
+            //get start and end addresses for every slot in current block
+            for(int j=0;j<currentlyUsedMemorySlots.size();j++) {
+                if(currentlyUsedMemorySlots.get(j).getStart()>=currentBlockStart && currentlyUsedMemorySlots.get(j).getEnd()<=currentBlockEnd) {
+                    startFlag.add(currentlyUsedMemorySlots.get(j).getStart());
+                    endFlag.add(currentlyUsedMemorySlots.get(j).getEnd());
+                }
+            }
+            
+            //All following ifs check if process fits in current block
+
+
+            //checks if process fits in block, only if there are no other slots in block
+            if(startFlag.isEmpty()) {
                 if(currentBlockEnd-currentBlockStart+1>=p.getMemoryRequirements()) {
                     fit = true;
                     address = currentBlockStart;
                 }
-            } else {
-
-                //get start and end addresses for every slot in current block
-                for(int j=0;currentlyUsedMemorySlots.get(j).getStart()<=currentBlockEnd;j++) {
-                    if(currentlyUsedMemorySlots.get(j).getStart()>=currentBlockStart) {
-                        startFlag.add(currentlyUsedMemorySlots.get(j).getStart());
-                        endFlag.add(currentlyUsedMemorySlots.get(j).getEnd());
-                    }
-                }
-                
-                //All following ifs check if process fits in current block
-                
-                //checks if process fits between start of block and start of first slot
+            }
+            
+            //checks if process fits between start of block and start of first slot
+            if(!fit) {
                 if(startFlag.get(0)-currentBlockStart>=p.getMemoryRequirements()) {
                     fit = true;
                     address = currentBlockStart; 
                 }
+            }
+            
 
-                //checks if process fits between any slot in block
-                if(!fit) {
-                    for(int j=1;j<startFlag.size() && !fit;j++) {
-                        if(startFlag.get(j)-endFlag.get(j-1)-1>=p.getMemoryRequirements()) {
-                            fit = true;
-                            address = endFlag.get(j-1)+1;
-                        }
-                    }
-                }
-
-                //checks if process fits between end of last slot and end of block
-                if(!fit) {
-                    if(currentBlockEnd-endFlag.get(endFlag.size()-1)>=p.getMemoryRequirements()) {
+            //checks if process fits between any slot in block
+            if(!fit) {
+                for(int j=1;j<startFlag.size() && !fit;j++) {
+                    if(startFlag.get(j)-endFlag.get(j-1)-1>=p.getMemoryRequirements()) {
                         fit = true;
-                        address = endFlag.get(endFlag.size()-1)+1;
+                        address = endFlag.get(j-1)+1;
                     }
                 }
             }
+
+            //checks if process fits between end of last slot and end of block
+            if(!fit) {
+                if(currentBlockEnd-endFlag.get(endFlag.size()-1)>=p.getMemoryRequirements()) {
+                    fit = true;
+                    address = endFlag.get(endFlag.size()-1)+1;
+                }
+            }
+            startFlag.clear();
+            endFlag.clear();
         }
+
+        //TEMPORARY TESTING CODE MEMORYSLOT ADDING
+        if(fit) {
+            MemorySlot slot = new MemorySlot(address, address+p.getMemoryRequirements()-1, currentBlockStart, currentBlockEnd);
+            if(currentlyUsedMemorySlots.isEmpty()) {
+                currentlyUsedMemorySlots.add(slot);
+            } else {
+                boolean inserted = false;
+               
+                for(int i=0;i<currentlyUsedMemorySlots.size() && !inserted;i++) {
+                    if(address<currentlyUsedMemorySlots.get(i).getStart()) {
+                        currentlyUsedMemorySlots.add(i, slot);
+                    }
+                }
+                if(!inserted) {
+                    currentlyUsedMemorySlots.add(slot);
+                }
+            }
+            
+        }
+        
+
         return address;
     }
 
