c0b5ab78de6b1c76cb2430d0a85b15c9f9abe10c
Eleni Mandana <elemandana@gmail.com>
2022-01-08 19:01:30 +0200

    mmu correction

--- MMU.java
+++ MMU.java
@@ -5,11 +5,22 @@ public class MMU {
     private final int[] availableBlockSizes;
     private MemoryAllocationAlgorithm algorithm;
     private ArrayList<MemorySlot> currentlyUsedMemorySlots;
-    
+
+    private ArrayList<Process> loadedProcesses = new ArrayList<>();
+
     public MMU(int[] availableBlockSizes, MemoryAllocationAlgorithm algorithm) {
         this.availableBlockSizes = availableBlockSizes;
         this.algorithm = algorithm;
         this.currentlyUsedMemorySlots = new ArrayList<MemorySlot>();
+    }
+
+    // getters
+    public ArrayList<Process> getLoadedProcesses(){
+        return this.loadedProcesses;
+    }   
+
+    public ArrayList<MemorySlot> getCurrentlyUsMemorySlots(){
+        return this.currentlyUsedMemorySlots;
     }
 
     public boolean loadProcessIntoRAM(Process p) {
@@ -18,8 +29,14 @@ public class MMU {
          * Hint: this should return true if the process was able to fit into memory
          * and false if not */
         int fits = algorithm.fitProcess(p, currentlyUsedMemorySlots);
-        if(fits == -1) { return true; }
+        
+        if(fits == -1) { 
+            loadedProcesses.add(p);
+            return true;
+        }
         
         return fit;
+
+        
     }
 }
