30e804cd5f387d4e445e32fce33c549b5e589f08
SpyrKarv <spyrkarv@csd.auth.gr>
2022-01-09 13:10:43 +0200

    Comments added. Make a working memory allocation algorithm people come on

--- CPU.java
+++ CPU.java
@@ -8,31 +8,35 @@ public class CPU {
     private Scheduler scheduler;
     private MMU mmu;
     private Process[] processes;
-    private int currentProcess;
-    private int previousProcess;
-    private Process currentProcessObject;
-    private Process previousProcessObject;
+    private int currentProcess; //PID of the current process.
+    private int previousProcess; //PID of the previous process.
+    private Process currentProcessObject; //The current process.
+    private Process previousProcessObject; //The previous process.
 
-    private ArrayList<Process> newProcesseses;
-    private ArrayList<Process> rejectedProcesses;
+    private ArrayList<Process> newProcesses; //Processes that have just been loaded in the RAM.
+    private ArrayList<Process> rejectedProcesses; //Processes that have failed to be loaded in the RAM.
 
-    private int processesCount;
+    private int processesCount; //The number of processes that haven't been terminated.
 
-    private boolean processRunning;
-    private int stateType;
+    private boolean processRunning; //If a process is running it's set to true.
+    private int stateType; //Information about the next tick.
 
     public CPU(Scheduler scheduler, MMU mmu, Process[] processes) {
         this.scheduler = scheduler;
         this.mmu = mmu;
         this.processes = processes;
     }
-    
+
+    /*
+    This is the program's main function. It initializes all needed variables and calls tick() in a loop
+    until all processes have been terminated.
+     */
     public void run() {
         /* TODO: you need to add some code here
          * Hint: you need to run tick() in a loop, until there is nothing else to do... */
         currentProcess = 0;
         currentProcessObject = null;
-        newProcesseses = new ArrayList<>();
+        newProcesses = new ArrayList<>();
         rejectedProcesses = new ArrayList<>();
         processesCount = processes.length;
         processRunning = false;
@@ -42,26 +46,43 @@ public class CPU {
             clock++;
         }
     }
-    
+
+    /*
+    The cpu's resources are managed in this function. Every time it's called, it decides what
+    the cpu will do. Past calls might affect feature ones.
+     */
     public void tick() {
         /* TODO: you need to add some code here
          * Hint: this method should run once for every CPU cycle */
+
         System.out.print("Tick: " + clock + " -- ");
+
+        //Checks if a process is supposed to arrive now. If it does, it tries to allocate RAM to it.
         for (Process process : processes)
             if (process.getArrivalTime() == clock) {
-                 scheduler.addProcess(process);
                  if (mmu.loadProcessIntoRAM(process))
-                    newProcesseses.add(process);
+                     newProcesses.add(process);
                  else
-                    rejectedProcesses.add(process);
+                     rejectedProcesses.add(process);
             }
+
+        /*
+        stateType = -1: Checks if there are processes in the state NEW. If there are and there is a process running,
+                        the stateType gets updated to 0 (in order to stop the current process at the next cycle).
+                        If there is no process running, the scheduler gets updated with the new processes.
+                        If there are no new processes, then the cpu continues running the current one (if there is a current one)
+                        by running checkCurrentProcess().
+        stateType = 0: The current process gets interrupted because there are new processes trying to get added in the scheduler.
+        stateType = 1: The process starts running.
+         */
         switch(stateType) {
             case -1:
-                if (newProcesseses.size() > 0) {
+                if (newProcesses.size() > 0) {
                     if (!processRunning) {
-                        System.out.println("Process " + newProcesseses.get(0).getPCB().getPid() + ": New -> Ready");
-                        newProcesseses.get(0).getPCB().setState(ProcessState.READY, clock);
-                        newProcesseses.remove(0);
+                        System.out.println("Process " + newProcesses.get(0).getPCB().getPid() + ": New -> Ready");
+                        newProcesses.get(0).getPCB().setState(ProcessState.READY, clock);
+                        scheduler.addProcess(newProcesses.get(0));
+                        newProcesses.remove(0);
                     }
                     else {
                         System.out.println("Waiting (Stopping Process)");
@@ -93,14 +114,22 @@ public class CPU {
                 stateType = -1;
                 break;
         }
+
+        /*At the end of a tick, the array which holds the processes that couldn't be loaded in the RAM gets checked in the event
+        that one of these processes can now be loaded (after a process has potentially been terminated).
+         */
         for (Process process : rejectedProcesses) {
             if (mmu.loadProcessIntoRAM(process)) {
-                newProcesseses.add(process);
+                newProcesses.add(process);
                 rejectedProcesses.remove(process);
             }
         }
     }
 
+    /*
+    Calculates the time that the current process has been running. If it's the same as its burstTime then the process
+    gets terminated, its allocated RAM is freed and the appropriate variables get updated.
+     */
     private void checkBurstTime() {
         int size = currentProcessObject.getPCB().getStartTimes().size();
         int timePassed = 0;
@@ -111,13 +140,15 @@ public class CPU {
                 timePassed += currentProcessObject.getPCB().getStopTimes().get(i) - (currentProcessObject.getPCB().getStartTimes().get(i) + 1);
         }
         if (timePassed == currentProcessObject.getBurstTime()) {
-            System.out.println("Process " + currentProcess + ": Running -> Terminated");
             ArrayList<MemorySlot> usedMemorySlots = mmu.getCurrentlyUsMemorySlots();
-            for (int i = 0; i < usedMemorySlots.size(); i++)
+            for (int i = 0; i < usedMemorySlots.size(); i++) {
                 if (usedMemorySlots.get(i).getStart() == currentProcessObject.getMemoryLocation()) {
                     usedMemorySlots.remove(i);
+                    for (MemorySlot mem : usedMemorySlots)
+                        System.out.println(mem.getStart() + " " + mem.getEnd());
                     break;
                 }
+            }
             currentProcessObject.getPCB().setState(ProcessState.TERMINATED, clock);
             scheduler.removeProcess(currentProcessObject);
             processesCount--;
@@ -127,6 +158,12 @@ public class CPU {
         }
     }
 
+    /*
+    Checks what the condition of the current process is and if it should stop, start or continue running.
+    First block: the process continues running.
+    Second block: the process starts running (stateType = 1).
+    Third block: the process that's running must be switched.
+     */
     private void checkCurrentProcess() {
         currentProcess = currentProcessObject.getPCB().getPid();
         if (currentProcess == previousProcess && processRunning) {
@@ -138,7 +175,7 @@ public class CPU {
             stateType = 1;
         }
         else {
-            System.out.println("Process " + previousProcess + ": Running -> Ready");
+            System.out.println("Process " + previousProcess + ": Running -> Ready (Switching)");
             previousProcessObject.waitInBackground();
             previousProcessObject.getPCB().setState(ProcessState.READY, clock);
             stateType = 1;
--- RoundRobin.java
+++ RoundRobin.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 public class RoundRobin extends Scheduler {
 
     private int quantum;
-    private Process currentProcess;
-    private int process_timer = 0;
-    private final ArrayList<Process> ReadyQueue = new ArrayList<>();
+    private Process currentProcess; //The process that's currently running.
+    private int process_timer = 0; //Quantum timer.
+    private final ArrayList<Process> ReadyQueue = new ArrayList<>(); //Processes that wait their turn to run.
 
     public RoundRobin() {
         this.quantum = 1; // default quantum
@@ -17,12 +17,24 @@ public class RoundRobin extends Scheduler {
         this.quantum = quantum;
     }
 
+    /*
+    Called when a new process must be added to the scheduler.
+    The process gets added to the processes and the ReadyQueue arrays.
+     */
     public void addProcess(Process p) {
         /* TODO: you need to add some code here */
         processes.add(p);
         ReadyQueue.add(p);
     }
-    
+
+    /*
+    Decides which process should run next.
+    If there are no processes then it returns null.
+    If there is no process running, the quantum timer reaches the quantum value or the current process is terminated,
+    then the process that's next in queue starts. If the quantum timer reached its value then the process that stopped
+    is added to the ReadyQueue.
+    If the process is in a RUNNING state the timer advances.
+     */
     public Process getNextProcess() {
         /* TODO: you need to add some code here
          * and change the return value */
