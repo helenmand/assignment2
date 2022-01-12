5b5a16c33b0f816e31d00d8b52adff5167a5612b
Eleni Mandana <elemandana@gmail.com>
2022-01-12 22:59:05 +0200

    best fit & worst fixed!

--- BestFit.java
+++ BestFit.java
@@ -14,71 +14,33 @@ public class BestFit extends MemoryAllocationAlgorithm {
          * loaded into if the process fits. In case the process doesn't fit, it
          * should return -1. */
 
-        // stores size of candidate blocks. 
-        // candidate blocks are those which are bigger or equal than the processes memory requirements.
-        ArrayList<Integer> candidate_blocks = new ArrayList<Integer>();
-        candidate_blocks = find_candidate_blocks(availableBlockSizes, p.getMemoryRequirements());
+        int minMax = 2147483647;
+        int block_index = 0;
 
-        // if there are no candidate blocks, the process does not fit
-        if (candidate_blocks.size() == 0) { return address; }
-        
         ArrayList<Integer[]> available_slots = new ArrayList<Integer[]>();
-        
-        for (Integer block : candidate_blocks){
-            // fininding start and end of the block
-            int block_index = find_index(availableBlockSizes, block);
+        Integer[] best_slots = new Integer[2];
+
+        for (int block : availableBlockSizes){
+            
             int block_start = 0;
-            
-            for (int i = 0; i < block_index; i++){ block_start += availableBlockSizes[i]; }
-            
+            for (int i = 0; i < block_index; i++){ block_start += availableBlockSizes[i]; } 
             int block_end = block_start + block - 1;
 
-            // getting the block's available slots  
             available_slots = getFreeSpaces(currentlyUsedMemorySlots, block_start, block_end);
 
             for (Integer[] slot : available_slots){
-                if (slot[1] - slot[0] + 1 >= p.getMemoryRequirements()) {
-                    address = slot[0];
+                if ((slot[1] - slot[0] + 1 >= p.getMemoryRequirements()) && (slot[1] - slot[0] + 1 < minMax)) {
+                    minMax = slot[1] - slot[0] + 1;
+                    best_slots = slot;
                     fit = true;
                 } 
             }
-
-            // if the process fits in this block, then there is no need to search other blocks.
-            if(fit){ break; }
+            ++block_index;
         }
-
+        if(fit){ address = best_slots[0]; }
         return address;
     }
 
-
-    /**
-     * finding every block that could fit the process. Adding the sizes of these blocks
-     * in an array list and sorting the list in ascending order
-     * 
-     * @param blocks every available block
-     * @param size the size of the process
-     * @return final_blocks, the blocks that fit the process
-     */
-    private ArrayList<Integer> find_candidate_blocks(int[] blocks, int size){
-        ArrayList<Integer> final_blocks = new ArrayList<Integer>();
-
-        for (int i = 0; i < blocks.length; i++){ if (blocks[i] >= size ){ final_blocks.add(blocks[i]);} }
-        final_blocks.sort(null);
-
-        // returns all candidate memory blocks in asceding order.
-        return final_blocks; 
-    }
-
-    /**
-     * returns the index of the given element in the given array.
-     * 
-     * @param arr the array 
-     * @param element the element
-     * @return the index of the element or -1 if it does not exist.
-     */
-    private int find_index(int[] arr, int element){
-        for (int i = 0; i < arr.length; i++){ if (arr[i] == element) { return i; } }   
-        return -1;
-    }
+    
     
 }
--- WorstFit.java
+++ WorstFit.java
@@ -14,89 +14,31 @@ public class WorstFit extends MemoryAllocationAlgorithm {
          * loaded into if the process fits. In case the process doesn't fit, it
          * should return -1. */
 
-        ArrayList<Integer> candidate_blocks = new ArrayList<Integer>();
-        candidate_blocks = find_candidate_blocks(availableBlockSizes, p.getMemoryRequirements());
-
-        // if there are no candidate blocks, the process does not fit
-        if (candidate_blocks.size() == 0) { return address; }
+        int maxMax = -1;
+        int block_index = 0;
 
         ArrayList<Integer[]> available_slots = new ArrayList<Integer[]>();
-        candidate_blocks = reverseArrayList(candidate_blocks);
-        
-        for (Integer block : candidate_blocks){
-            // fininding start and end of the block
-            int block_index = find_index(availableBlockSizes, block);
+        Integer[] best_slots = new Integer[2];
+
+        for (int block : availableBlockSizes){
+            
             int block_start = 0;
-            
-            for (int i = 0; i < block_index; i++){ block_start += availableBlockSizes[i]; }
-            
+            for (int i = 0; i < block_index; i++){ block_start += availableBlockSizes[i]; } 
             int block_end = block_start + block - 1;
 
-            // getting the block's available slots  
             available_slots = getFreeSpaces(currentlyUsedMemorySlots, block_start, block_end);
 
             for (Integer[] slot : available_slots){
-                if (slot[1] - slot[0] + 1 >= p.getMemoryRequirements()) {
-                    address = slot[0];
+                if ((slot[1] - slot[0] + 1 >= p.getMemoryRequirements()) && (slot[1] - slot[0] + 1 > maxMax)) {
+                    maxMax = slot[1] - slot[0] + 1;
+                    best_slots = slot;
                     fit = true;
                 } 
             }
-
-            // if the process fits in this block, then there is no need to search other blocks.
-            if(fit){ break; }
+            ++block_index;
         }
+        if(fit){ address = best_slots[0]; }
         return address;
     }
     
-    
-    /**
-     * finding every block that could fit the process. Adding the sizes of these blocks
-     * in an array list and sorting the list in ascending order
-     * 
-     * @param blocks every available block
-     * @param size the size of the process
-     * @return final_blocks, the blocks that fit the process
-     */
-    private ArrayList<Integer> find_candidate_blocks(int[] blocks, int size){
-        ArrayList<Integer> final_blocks = new ArrayList<Integer>();
-
-        for (int i = 0; i < blocks.length; i++){ if ( final_blocks.get(i) >= size ){ final_blocks.add(blocks[i]);} }
-        final_blocks.sort(null);
-
-        // returns all candidate memory blocks in asceding order.
-        return final_blocks; 
-    }
-
-    /**
-     * returns the index of the given element in the given array.
-     * 
-     * @param arr the array 
-     * @param element the element
-     * @return the index of the element or -1 if it does not exist.
-     */
-    private int find_index(int[] arr, int element){
-        for (int i = 0; i < arr.length; i++){ if (arr[i] == element) { return i; } }   
-        return -1;
-    }
-
-    /**
-     * returns an array list in reversed order
-     * src : https://www.geeksforgeeks.org/reverse-an-arraylist-in-java/
-     * 
-     * @param alist array list to be reversed
-     * @return reversed array list
-     */
-    private ArrayList<Integer> reverseArrayList(ArrayList<Integer> alist) {
-        // Arraylist for storing reversed elements
-        // this.revArrayList = alist;
-        for (int i = 0; i < alist.size() / 2; i++) {
-            Integer temp = alist.get(i);
-            alist.set(i, alist.get(alist.size() - i - 1));
-            alist.set(alist.size() - i - 1, temp);
-        }
- 
-        // Return the reversed arraylist
-        return alist;
-    }
-
 }
