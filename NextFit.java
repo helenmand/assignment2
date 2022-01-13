ec3781d6f219e8904bfb4a922470ebc88f9c7482
AngelopoulosD <dpangelo@csd.auth.gr>
2022-01-13 19:30:45 +0200

    Corrected the method fitProcess to manages the free spaces returned by the getFreeSpaces method of class MemoryAllocationAlgorithm to finds a free space in the memory and removed the getSlots method

--- NextFit.java
+++ NextFit.java
@@ -9,38 +9,7 @@ public class NextFit extends MemoryAllocationAlgorithm {
     public NextFit(int[] availableBlockSizes) {
         super(availableBlockSizes);
     }
-    
-    /*
-     * This method finds the slots in a specific block
-     * 
-     * @param blockSlots is an array that contains all the memory slots
-     * @param endBlock is an integer that indicates which of the blocks we want the slots for
-     * 
-     * @return an array that includes the slots of the block
-     */
-    private ArrayList<MemorySlot> getSlots(ArrayList<MemorySlot> blockSlots, int endBlock){
-		ArrayList<MemorySlot> slots= new ArrayList<>();
-		int slotSize=blockSlots.size();
-		// There is not slots
-		if(slotSize==0) {
-			return null;
-		}
-		else {// there is some slots
-			int index=0;
-			MemorySlot slot;
-			while(index<slotSize) {
-				slot=blockSlots.get(index);
-				// checks if the slot is in the specific block and if the address is behind of the slot
-				if(slot.getBlockEnd()==endBlock && this.address<=slot.getEnd()) {
-					slots.add(slot);
-					}
-				index+=1;
-				}
-			return slots;
-		}	
-    }
-
-    
+     
     public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots) {
         boolean fit = false;
         int address = -1;
@@ -54,77 +23,53 @@ public class NextFit extends MemoryAllocationAlgorithm {
         for(int i=0;i<availableBlockSizes.length;i++) {
     		sumAllBlock+=availableBlockSizes[i];
     	}
-        // where the address starts
-        int startAddress=this.address;
+        // where the address will stop looking for free space
+        int endAddress=this.address+sumAllBlock;
+        // 
+        int countEndAddress=this.address;
         
-        ArrayList<MemorySlot> blockSlots= new ArrayList<>();
-        // takes the slots of the block we are
-        blockSlots=getSlots(currentlyUsedMemorySlots, s-1);
+        ArrayList<Integer[]> freePositions= new ArrayList<>();
+        // takes the free spaces of the block
+        freePositions=getFreeSpaces(currentlyUsedMemorySlots, this.address, s-1);
         int index=0;
         
         do {
-        	// This block has no slots or the address is in front of the slots
-        	if(blockSlots==null || blockSlots.size()==0) {
-        		// checks if the process can be entered into memory
-        		if(s-this.address>=p.getMemoryRequirements()) {
-        			// the address at which the process space begins
+        	// checks if the block has free spaces 
+        	if(index<freePositions.size()) { 
+        		// checks if the process can be entered to this free space
+        		//        end free space      -     start free space   (+1 because the same space counts for a free space)
+        		if(freePositions.get(index)[1]-freePositions.get(index)[0]+1>=p.getMemoryRequirements()) {
         			address=this.address;
         			fit=true;
-        		}	
+        			this.address+=p.getMemoryRequirements()-1;
+        		}
+        		else { 
+        				// the address moves to the end of this free space
+        				countEndAddress+=(freePositions.get(index)[1]-this.address);
+        				this.address=freePositions.get(index)[1];
+        				index+=1;
+    			}
         	}
-        	// That block has slots
-        	else {
-        		MemorySlot slot=blockSlots.get(index);
-        		
-        		// checks if the slot is finished and if there is another slot 
-    			if(this.address==slot.getEnd() && blockSlots.size()>index+1) {
-    				index+=1;
-    			}
-    			else {// checks if the address does not point to used space
-    				// behind of slot
-    				if(this.address<slot.getStart()) {
-    					if(slot.getStart()-this.address>=p.getMemoryRequirements()) {
-    						// the address at which the process space begins
-    	        			address=this.address;
-    	        			fit=true;
-    					}
-    				}
-    				else {
-    					// front of slot
-    	        		if(this.address>slot.getEnd()) {
-    	        			// checks the empty space if the process can be entered
-    	        			if(s-this.address>=p.getMemoryRequirements()) {
-    	        				address=this.address;
-    	        				fit=true;
-    	        			}
-    	        		}
-    				}
-    			}
+        	else {// the block not has free spaces
+        		countEndAddress+=s-this.address;
+        		// checks if it was the last block for to start from the first memory block
+        		if(block+1==this.availableBlockSizes.length) {
+        			block=0;
+        			s=availableBlockSizes[block];
+        			this.address=0;
+        		}
+        		else {// not was the last block, next block
+        			block+=1;
+        			this.address=s;
+        			s+=availableBlockSizes[block];
+        		}
+        		// takes the free spaces of the block
+        		freePositions=getFreeSpaces(currentlyUsedMemorySlots, this.address, s-1);
+        		index=0;
         	}
         	
-        	// Updates the address
-        	if(!fit) {
-        		this.address=(this.address+1)%sumAllBlock;
-        	}
-        	// return to the first block
-    		if(this.address==0) {
-    			block=0;
-    			s=availableBlockSizes[block];
-    			blockSlots=getSlots(currentlyUsedMemorySlots, s-1);
-    			index=0;
-    		}
-    		else {
-    			// next block
-    			if(this.address==s) {
-	    			block+=1;
-	    			s+=availableBlockSizes[block];
-	    			blockSlots=getSlots(currentlyUsedMemorySlots, s-1);
-	    			index=0;
-    			}
-    			
-    		}
-        }while(startAddress!=this.address && !fit);
-
+        }while(endAddress>countEndAddress && !fit);
+        
         return address;
     }
 
