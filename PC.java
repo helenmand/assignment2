07be26426c1a4f3fef3706afa9a866b0fc0e8364
Christos Apostolidis <chriapos@csd.auth.gr>
2022-01-23 23:35:00 +0200

    Added testing prints in PC

--- PC.java
+++ PC.java
@@ -1,4 +1,3 @@
-
 public class PC {
 
     public static void main(String[] args) {
@@ -6,22 +5,30 @@ public class PC {
         
         final Process[] processes = {
                 // Process parameters are: arrivalTime, burstTime, memoryRequirements (kB)
-                new Process(5, 21, 26, 0),
-                new Process(12, 12, 40, 1),
-                new Process(20, 19, 42, 2),
-                new Process(31, 9, 30, 3),
-                new Process(32, 13, 13, 4),
-                new Process(32, 15, 24, 5),
-                new Process(32, 19, 39, 6),
-                new Process(32, 8, 6, 7),
-                new Process(32, 9, 3, 8)
+                new Process(5, 9, 10, 1),
+                new Process(12, 12, 40, 2),
+                new Process(20, 7, 25, 3),
+                new Process(31, 8, 30, 4)
         };
-        final int[] availableBlockSizes = {15, 40, 50, 25}; // sizes in kB
-        MemoryAllocationAlgorithm algorithm = new FirstFit(availableBlockSizes);
+        final int[] availableBlockSizes = {15, 40, 50, 60}; // sizes in kB
+        MemoryAllocationAlgorithm algorithm = new NextFit(availableBlockSizes);
         MMU mmu = new MMU(availableBlockSizes, algorithm);        
         Scheduler scheduler = new FCFS();
         CPU cpu = new CPU(scheduler, mmu, processes);
         cpu.run();
+
+        for(Process p : processes) {
+            System.out.println("");
+            System.out.println("--PROCESS " + p.getPCB().getPid() + "---");
+            System.out.println("Arrival Time: " + p.getArrivalTime());
+            System.out.println("Burst Time: " + p.getBurstTime());
+            System.out.println("Memory Requirements: " + p.getMemoryRequirements());
+            System.out.println("Memory Location: " + p.getMemoryLocation());
+            System.out.println("Waiting Time: " + p.getWaitingTime());
+            System.out.println("Response Time: " + p.getResponseTime());
+            System.out.println("Turnaround Time: " + p.getTurnAroundTime());
+            System.out.println("");
+        }
     }
 
 }
