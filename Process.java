45fdca5ffef7d06de7f9885b291d23cacc474577
Christos Apostolidis <chriapos@csd.auth.gr>
2022-01-23 23:35:30 +0200

    Fixed getTurnAroundTime()

--- Process.java
+++ Process.java
@@ -86,7 +86,7 @@ public class Process {
     public double getTurnAroundTime() {
         /* TODO: you need to add some code here
          * and change the return value */
-        return pcb.getInitialReadyTime() - pcb.getStopTimes().get(-1);
+        return pcb.getStopTimes().get(pcb.getStopTimes().size()-1) - pcb.getInitialReadyTime();
     }
 
     
